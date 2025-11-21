6a56ea5ea919a1d5c0d61ad696722e7b571a4330
uma <chernoetimon@gmail.com>
2025-11-21 14:55:39 +0300

    an app that calculates the number of miles accrued for a purchased ticket

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int coast = 100;
+        int spend = 100;
         int storage = 20;
-        int bonus;
-        System.out.println("За вашу покупку начилиться: " + (bonus = coast/storage) + " бонусов");
+        int bonus =  spend/storage;
+        System.out.println("За вашу покупку начилиться: " + bonus + " бонусов");
     }
 }
